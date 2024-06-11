2bf18c7f18fbeecfd04bdde5d7f5145ffb650f29
Yahya Zht <zhtyahya@gmail.com>
2024-06-11 14:14:00 +0200

    comment  'expires_in' => auth()->factory()->getTTL() * 60 * 24,

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -88,7 +88,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60 * 24,
+            // 'expires_in' => auth()->factory()->getTTL() * 60 * 24,
             'user' => auth()->user()->CIN,
             'Role' => auth()->user()->personne->role
         ]);
